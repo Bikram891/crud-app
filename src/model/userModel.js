b2108372a4609de6561f28f76d8bc7fc1ad7cb46
Bikram891 <bikram.s@dntinfotech.com>
2024-12-09 13:22:42 +0530

    Update userModel.js

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose');
+let userSchema = new mongoose.Schema ({
+    name:{ type: String,
+    required: [true, "Name is required"],
+    unique: true,
+    trim: true,
+    minlength: [2, "Name must be at least 2 characters long"],
+    maxlength: [32, "Name must be at most 32 characters long"],
+    },
+    email:{ type: String,
+        required: [true, "Email is required"],
+        unique: true, 
+        trim: true,
+        match: [/.+@.+\..+/, "Email must be a valid format"],
+      },
+    address:{type:String}
+})
 
+module.exports = mongoose.model('users',userSchema);
