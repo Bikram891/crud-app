d2b6c3235380d4f3326de75b4d0f41cea1aee2b9
Bikram891 <bikram.s@dntinfotech.com>
2024-12-10 14:19:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-require('dotenv').config();
 const express = require('express');
+const dotenv = require('dotenv');
 const connectDB = require('./src/config/db');
-const app = express();
 const userRoutes = require('./src/router/userRouter');
+const errorHandler = require('./src/middleware/errorHandler');
 
-
-
-app.use(express.json());
-
-// Routes
-app.use('/api/users', userRoutes);
-
+dotenv.config();
 connectDB();
 
-///-------------SERVER START-------------------------///
+const app = express();
+app.use(express.json());
+app.use('/api/users', userRoutes);
+app.use(errorHandler);
+
+// ///-------------SERVER START-------------------------///
 const PORT = process.env.PORT || 3500;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
