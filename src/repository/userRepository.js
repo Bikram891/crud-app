09a11a47d0516d91d8086350f3e5ef9a6582dd42
Bikram891 <bikram.s@dntinfotech.com>
2024-12-10 14:16:35 +0530

    Update userRepository.js

--- src/repository/userRepository.js
+++ src/repository/userRepository.js
@@ -1,73 +1,43 @@
 const User = require('../models/userModel');
 
-class UserRepository {
-  
-  async getAllUsers() {
-    try {
-      return await User.find();
-    } catch (error) {
-      throw new Error('Error fetching all users: ' + error.message);
+const createUser = async (userData) => {
+  try {
+    return await User.create(userData);
+  } catch (error) {
+    if (error.code === 11000) {
+      throw new Error('Email already exists');
     }
+    throw error;
   }
+};
 
-  async createUser(userData) {
-    try {
-      return await User.create(userData);
-    } catch (error) {
-      throw error; 
-    }
-  }
+const getUserByEmail = async (email) => {
+  return await User.findOne({ email });
+};
 
-  async getUserById(id) {
-    try {
-      const user = await User.findById(id);
-      if (!user) throw new Error('User not found');
-      return user;
-    } catch (error) {
-      throw new Error('Error fetching user by ID: ' + error.message);
-    }
-  }
+const getUserById = async (id) => {
+  return await User.findById(id);
+};
 
-  async updateUser(id, updateData) {
-    try {
-      const user = await User.findByIdAndUpdate(id, updateData,
-         {
-           new: true,
-           runValidators: true
-           });
-      if (!user) throw new Error('User not found');
-      return user;
-    } catch (error) {
-      throw new Error('Error updating user: ' + error.message);
-    }
-  }
+const getAllUsers = async () => {
+  return await User.find();
+};
 
-  async patchUpdateUser(id, updateData) {
-    try {
-      const user = await User.findByIdAndUpdate(id, updateData, { new: true, runValidators: true });
-      if (!user) throw new Error('User not found');
-      return user;
-    } catch (error) {
-      throw new Error('Error patch updating user: ' + error.message);
-    }
+const updateUser = async (id, updateData) => {
+  try {
+    return await User.findByIdAndUpdate(id, updateData, { new: true });
+  } catch (error) {
+    throw new Error('Error updating user: ' + error.message);
   }
+};
 
-  async deleteUser(id) {
-    try {
-      return await User.findByIdAndDelete(id);
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async deleteMany() {
-    try {
-      return await User.deleteMany();
-    } catch (error) {
-      throw new Error('Error deleting users: ' + error.message);
-    }
-  }
+const deleteMany = async () =>{
+  return await User.deleteMany({});
 }
 
-module.exports = new UserRepository();
+const deleteUserById = async (id) =>{
+  return await User.findByIdAndDelete(id);
+}
+
+module.exports = { createUser, getUserByEmail, getUserById, getAllUsers, updateUser,deleteMany, deleteUserById};
 
