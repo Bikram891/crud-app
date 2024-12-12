f4c9e3a3ba2302826dd2e5c1e0457b70b5c816e2
Bikram891 <bikram.s@dntinfotech.com>
2024-12-12 11:19:28 +0530

    Update userRouters.js

--- src/router/userRouters.js
+++ src/router/userRouters.js
@@ -1,40 +1,18 @@
 const express = require('express');
-const { body, param } = require('express-validator');
 const validate = require('../middleware/valiadte');
 const userController = require('../controller/userController');
+const validateCreateUser = require('../middleware/validation/createUserValidation');
+const validateUpdateUser = require('../middleware/validation/updateUserValidation');
+const validateGetUser = require('../middleware/validation/getIdUserValidation');
+const validateDeleteUser = require('../middleware/validation/deleteUserValidation');
 
 const router = express.Router();
 
-router.post('/create',validate([
-    body('email').isEmail().withMessage('Invalid email format'),
-    body('name')
-    .isLength({ min: 2, max: 32 })
-    .withMessage('Name must be between 2 and 32 characters')
-    .notEmpty()
-    .withMessage('Name is required'),
-    body('address')
-  ]),
-  userController.createUser
-);
-router.put(
-    '/:id',
-    validate([
-      param('id').isMongoId().withMessage('Invalid user ID'),
-      body('email')
-        .optional()
-        .isEmail()
-        .withMessage('Invalid email format'),
-      body('name')
-        .optional()
-        .isLength({ min: 2, max: 32 })
-        .withMessage('Name must be between 2 and 32 characters'),
-      body('address'), 
-    ]),
-    userController.updateUser
-  );
+router.post('/create',validateCreateUser,validate,userController.createUser);
+router.put('/:id',validate,validateUpdateUser, userController.updateUser);
 router.get('/all', userController.getAllUsers);
-router.get('/:id',userController.getUserById);
+router.get('/:id',validate, validateGetUser,userController.getUserById);
 router.delete('/',userController.deleteAllUsers);
-router.delete('/:id',userController.deleteUser)
+router.delete('/:id',validate,validateDeleteUser,userController.deleteUser);
 
 module.exports = router;
