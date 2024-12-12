3bfed62c53f209c3f7855a705ac9ec5054de1afd
Bikram891 <bikram.s@dntinfotech.com>
2024-12-12 11:14:25 +0530

    Update validate.js

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -1,18 +1,15 @@
 const { validationResult } = require('express-validator');
 
-const validate = (validations) => {
-  return async (req, res, next) => {
-    await Promise.all(validations.map(validation => validation.run(req)));
+const validate = (req, res, next) => {
     const errors = validationResult(req);
-
     if (!errors.isEmpty()) {
-      return res.status(400).json({
-        success: false,
-        errors: errors.array(),
-      });
+        return res.status(400).json({
+          code: 400,
+          status: 'Bad Request',
+          errors: errors.array().map(error => error.msg)
+        })
     }
     next();
-  };
 };
 
 module.exports = validate;
