9c05fb9ccd61461cc40c03c2ace211fb3f5dd352
Bikram891 <bikram.s@dntinfotech.com>
2024-12-13 17:46:02 +0530

    Update userModel.js

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,12 +1,29 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     address: { type: String },
+    password:{type:String , required:true}
   },
   { timestamps: true }
 );
 
+// Hash password before saving to DataBase
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+
+// Compare password for login
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+
 module.exports = mongoose.model('User', userSchema);
+
