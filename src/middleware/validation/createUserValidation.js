832b0be907776ca61953193eb4eff1bcdcdaecef
Bikram891 <bikram.s@dntinfotech.com>
2024-12-13 17:48:30 +0530

    Update createUserValidation.js

--- src/middleware/validation/createUserValidation.js
+++ src/middleware/validation/createUserValidation.js
@@ -1,9 +1,10 @@
-const { body} = require('express-validator');
+const { body,check} = require('express-validator');
 
 const validateCreateUser = [
     body('name').isLength({ min: 2, max: 32 }).withMessage('Name must be between 2 and 32 characters').notEmpty().withMessage('Name is required'),
     body('email').isEmail().withMessage('Valid email is required'),
-    body('address')
+    body('address'),
+    check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
   ];
 
 module.exports = validateCreateUser;
