3c8ae16605852b9c2518bd3de5079f4069e12002
Bikram891 <bikram.s@dntinfotech.com>
2024-12-17 18:21:26 +0530

    Update userAuthRouter.js

--- src/router/userAuthRouter.js
+++ src/router/userAuthRouter.js
@@ -1,9 +1,15 @@
 const loginUser = require('../controller/userAuthLoginContrller');
 const validateLogin  =require('../middleware/validation/authValidation');
 const validate = require('../middleware/valiadte');
+const validateCreateUser = require('../middleware/validation/createUserValidation');
+const { createUser } = require('../controller/userController');
 const express = require('express');
 const router = express.Router();
 
-router.post('/login',validateLogin,validate,loginUser);
+
+
+router.post('/login', validateLogin, validate, loginUser);
+router.post('/create', validateCreateUser, validate, createUser);
+
 
 module.exports = router;
