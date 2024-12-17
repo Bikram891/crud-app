c70239392de4d0ce3b854338d5089603d664fd20
Bikram891 <bikram.s@dntinfotech.com>
2024-12-17 18:23:53 +0530

    Update userRepository.js

--- src/repository/userRepository.js
+++ src/repository/userRepository.js
@@ -12,20 +12,20 @@ const createUser = async (userData) => {
 };
 
 const getUserByEmail = async (email) => {
-  return await User.findOne({ email });
+  return await User.findOne({ email }).select('-password');
 };
 
 const getUserById = async (id) => {
-  return await User.findById(id);
+  return await User.findById(id).select('-password');
 };
 
 const getAllUsers = async () => {
-  return await User.find();
+  return await User.find().select('-password');
 };
 
 const updateUser = async (id, updateData) => {
   try {
-    return await User.findByIdAndUpdate(id, updateData, { new: true });
+    return await User.findByIdAndUpdate(id, updateData, { new: true }).select('-password');
   } catch (error) {
     throw new Error('Error updating user: ' + error.message);
   }
@@ -36,8 +36,17 @@ const deleteMany = async () =>{
 }
 
 const deleteUserById = async (id) =>{
-  return await User.findByIdAndDelete(id);
+  return await User.findByIdAndDelete(id).select('-password');
 }
 
-module.exports = { createUser, getUserByEmail, getUserById, getAllUsers, updateUser,deleteMany, deleteUserById};
+module.exports = {
+  createUser,
+  getUserByEmail,
+  getUserById,
+  getAllUsers,
+  updateUser,
+  deleteMany,
+  deleteUserById
+};
+
 
