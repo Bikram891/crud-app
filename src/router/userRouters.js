d917f329fae77d5a529337d7c9daf196afb66969
Bikram891 <bikram.s@dntinfotech.com>
2024-12-17 18:22:01 +0530

    Update userRouters.js

--- src/router/userRouters.js
+++ src/router/userRouters.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const validate = require('../middleware/valiadte');
 const userController = require('../controller/userController');
-const validateCreateUser = require('../middleware/validation/createUserValidation');
 const validateUpdateUser = require('../middleware/validation/updateUserValidation');
 const validateGetUser = require('../middleware/validation/getIdUserValidation');
 const validateDeleteUser = require('../middleware/validation/deleteUserValidation');
-
 const router = express.Router();
+const authorizeRole = require('../middleware/authorizeRole');
 
-router.post('/create',validateCreateUser,validate,userController.createUser);
-router.put('/:id',validate,validateUpdateUser, userController.updateUser);
-router.get('/all', userController.getAllUsers);
-router.get('/:id',validate, validateGetUser,userController.getUserById);
-router.delete('/',userController.deleteAllUsers);
-router.delete('/:id',validate,validateDeleteUser,userController.deleteUser);
+router.get('/all',authorizeRole('Admin'), userController.getAllUsers);
+router.put('/:id',authorizeRole('Admin','User'),validate,validateUpdateUser, userController.updateUser);
+router.get('/:id',authorizeRole('Admin','User'),validate,validateGetUser,userController.getUserById);
+router.delete('/',authorizeRole('Admin'),userController.deleteAllUsers);
+router.delete('/:id',authorizeRole('Admin'),validate,validateDeleteUser,userController.deleteUser);
 
 module.exports = router;
