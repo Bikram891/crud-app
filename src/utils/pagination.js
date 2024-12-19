6147ae3d2a9aa37cc80e430139e64ab14e902e38
Bikram891 <bikram.s@dntinfotech.com>
2024-12-19 11:03:21 +0530

    Update pagination.js

--- src/utils/pagination.js
+++ src/utils/pagination.js
@@ -2,17 +2,17 @@ const paginate = async (model, page = 1, limit = 10, excludeFields = []) => {
     try {
       const startIndex = (page - 1) * limit;
   
-      // Exclude fields, e.g., password
+    
        const excludedFields = excludeFields.map((field) => `-${field}`).join(' ');
         
-      // Fetch data with pagination
+     
       const data = await model
         .find()
         .skip(startIndex)
         .limit(limit)
         .select(excludedFields);
   
-      // Total count of documents
+     
       const totalDocuments = await model.countDocuments();
   
       return {
